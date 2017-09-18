cf5a7a25d8a8c0b58c9c188866b6f03f800aaafb
robotenique <julianogarcia_1997@hotmail.com>
2017-09-17 23:46:36 -0300

    modify a little..

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -3,5 +3,10 @@ use chrono::*;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    println!("{:?}", start);
     unimplemented!()
+}
+
+pub fn main() {
+    println!("HMMM");
 }
