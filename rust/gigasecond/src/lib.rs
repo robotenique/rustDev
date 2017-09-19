11e12de6f85bd259f798af147ddc0c61fb6f4d33
robotenique <julianogarcia_1997@hotmail.com>
2017-09-19 20:04:00 -0300

    finish gigasecond exercise

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,12 +1,10 @@
 extern crate chrono;
 use chrono::*;
 
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    println!("{:?}", start);
-    unimplemented!()
-}
-
-pub fn main() {
-    println!("HMMM");
+    // The Duration is from chrono crate, not to confuse with std::time:Duration
+    let dur = Duration::seconds(1e9 as i64);
+    start + dur
 }
