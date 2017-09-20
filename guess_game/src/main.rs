c133cdc10c24f841387203c3cc0a18c24b0ce6e8
robotenique <julianogarcia_1997@hotmail.com>
2017-09-20 20:16:17 -0300

    add final version of the guessing game >:D

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -3,32 +3,37 @@ extern crate rand;
 use std::io;
 use rand::*;
 use std::cmp::Ordering;
-use rand::distributions::IndependentSample;
 
 fn main() {
     let secret_number0 = rand::thread_rng().gen_range(1, 101);
-    let secret_number1 = rand::distributions::normal::Normal::new(50.0, 50.0)
-                        .ind_sample(&mut rand::thread_rng());
     println!("Guess what number I'm thinking 🤔");
-    println!("The secret number (0) is {}.", secret_number0);
-    //println!("The ULTRA secret number (1) is {}.", secret_number1 as i64);
-    println!("Type your guess.");
+    // println!("The secret number (0) is {}.", secret_number0);
+    loop {
+        println!("Type your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to readline D::");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to readline D::");
 
-    let guess: u32 = guess.trim().parse()
-                .expect("Please... TYPE A NUMBER!!");;
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please... TYPE A NUMBER!!");
+                continue;
+            }
+        };
 
-    println!("You guessed: {} !", guess);
+        println!("You guessed: {} !", guess);
 
-    match guess.cmp(&secret_number0) {
-        Ordering::Less  => println!("Too small! D:"),
-        Ordering::Greater   => println!("Too big! >:("),
-        Ordering::Equal  => println!("You got in right! :D")
+        match guess.cmp(&secret_number0) {
+            Ordering::Less  => println!("Too small! D:"),
+            Ordering::Greater   => println!("Too big! >:("),
+            Ordering::Equal  => {
+                println!("You got in right! :D");
+                break;
+            }
+        }
     }
-
 
 }
