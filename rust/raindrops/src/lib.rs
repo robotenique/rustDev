5a508fdd2e3af677989595a9da5718258e25a574
robotenique <julianogarcia_1997@hotmail.com>
2017-09-22 16:08:53 -0300

    finish raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,20 @@
-pub fn raindrops(n: usize) -> String {
 
+pub fn raindrops(n: usize) -> String {
+    let mut f = 1;
+    let mut my_str = String::new();
+    while f <= n {
+        if n % f == 0 {
+            my_str.push_str(match f {
+                3 => "Pling",
+                5 => "Plang",
+                7 => "Plong",
+                _ => ""
+            });
+        }
+        f += 1;
+    }
+    match my_str == "" {
+        true => n.to_string(),
+        false => my_str
+    }
 }
